885f47bca6cf69e87de4f7f5f57118017663d44f
Kamie Short <kamie.short@gmail.com>
2022-01-20 12:23:51 -0800

    step 18-19: added variables pull DOM elements

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 // import functions
-import { people } from './data.js';
-console.log(people, 'people');
+import { people } from "./data.js";
+console.log(people, "people");
 // grab DOM elements
+const selectEl = document.getElementById("name");
+const nameEl = document.querySelector(".name");
+const ageEl = document.querySelector(".age");
+const bioEl = document.querySelector(".bio");
+const hobbiesEl = document.querySelector(".hobbies");
+const contain = document.querySelector(".profile");
 
-// set event listeners 
-    // get user input
-    // use user input to update state 
-    // update DOM to reflect the new state
+console.log ("elements", selectEl, nameEL, ageEL, bioEl, hobbiesEl, contain);
+
+
+// set event listeners
+// get user input
+// use user input to update state
+// update DOM to reflect the new state
