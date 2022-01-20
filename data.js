747d838220edc6354267bd40350f1a07752d87ae
Kamie Short <kamie.short@gmail.com>
2022-01-19 17:03:10 -0800

    added id and array for profiles

--- data.js
+++ data.js
@@ -1,23 +1,23 @@
 export const people = [
- {
-    id: 1,
-    name: 'Bradley',
-    age: 32,
-    bio: 'I play a lot of video games.',
-    hobbies: ['video games', 'play guitar', 'eat lots of pizza'],
-},
+    {
+        id: 1,
+        name: 'Bradley',
+        age: 32,
+        bio: 'I play a lot of video games.',
+        hobbies: ['video games', 'play guitar', 'eat lots of pizza'],
+    },
 
-{id: 2,
-name: 'Mary',
-age: 30,
-bio: 'I got nothin\'',
-hobbies:['baby', 'painting', 'audiobooks'],
-},
+    { id: 2,
+        name: 'Mary',
+        age: 30,
+        bio: 'I got nothin\'',
+        hobbies:['baby', 'painting', 'audiobooks'],
+    },
 
-{id: 3,
-    name: 'Kamie',
-    age: 31,
-    bio: 'My wife and my dogs are my best friends',
-    hobbies:['campervan', 'hiking', 'parenting']
-},
+    { id: 3,
+        name: 'Kamie',
+        age: 31,
+        bio: 'My wife and my dogs are my best friends',
+        hobbies:['campervan', 'hiking', 'parenting']
+    },
 ];
